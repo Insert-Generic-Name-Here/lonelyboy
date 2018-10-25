222376d12e262854e8b2d5ffbbc6c27f26bd4437
theo <giorgostheo97@gmail.com>
2018-10-25 17:40:13 +0300

    Added comments and docstrings.

--- geospatial/preprocessing.py
+++ geospatial/preprocessing.py
@@ -9,14 +9,21 @@ import numpy as np
 
 
 def distance_to_nearest_port(point, ports):
+				'''
+				Calculates the minimum distance between the point and the lists of ports. Can be used to determine if the ship is sailing or not
+				'''
 				return ports.geom.distance(point).min()
 
 
 def get_outliers(series, alpha = 3):
+				'''
+				Returns a series of indexes of row that are to be concidered outliers, using the quantilies of the data. 
+				'''
 				q25, q75 = series.quantile((0.25, 0.75))
 				iqr = q75 - q25
 				q_high = q75 + alpha*iqr
 				q_low = q25 - alpha*iqr
+				# return the indexes of rows that are over/under the threshold above
 				return series.loc[(series >q_high) | (series<q_low)].index
 
 
@@ -25,12 +32,17 @@ def resample_geospatial(sample_ves, rule = '60S', method='linear', crs = {'init'
 				Resample and interpolate linearly a sample vessel.
 
 				'''
+				#convert unix to datetime
 				sample_ves['datetime'] = pd.to_datetime(sample_ves['ts'],unit='s')
+				#resample and interpolate using the method given. Linear is suggested
 				upsampled = sample_ves.resample(rule,on='datetime', loffset=True, kind='timestamp').first()
 				interpolated = upsampled.interpolate(method=method)
+				#interpolate the geom column with the correct point objects using lat and lon
 				interpolated['geom'] = interpolated[['lon', 'lat']].apply(lambda x: Point(x[0], x[1]), axis=1)
+				# reset the index to normal and use the old index as new timestamp
 				interpolated['datetime'] = interpolated.index
 				interpolated.reset_index(drop=True, inplace=True)
+				#drop lat and lon if u like
 				if drop_lon_lat:
 								interpolated.drop(['lat', 'lon'], axis=1)
 				return gpd.GeoDataFrame(interpolated, crs = crs, geometry='geom')
@@ -39,11 +51,14 @@ def resample_geospatial(sample_ves, rule = '60S', method='linear', crs = {'init'
 def calculate_velocity(gdf):
 				'''
 				Return given dataframe with an extra velocity column that is calculated using the distance covered in a given amount of time
+				TODO - use the get distance method to save some space
 				'''
+				#create columns for current and next location. Drop the last columns that contains the nan value
 				gdf['current_loc'] = gdf.geom.apply(lambda x: (x.x,x.y))
 				gdf['next_loc'] = gdf.geom.shift(-1)
 				gdf = gdf[:-1]
 				gdf['next_loc'] = gdf.next_loc.apply(lambda x : (x.x,x.y))
+				# get the distance traveled in n-miles and multiply by the rate given (3600/secs for knots) 
 				gdf['velocity'] = gdf[['current_loc', 'next_loc']].apply(lambda x : haversine(x[0], x[1])*0.539956803 , axis=1).multiply(3600/gdf.ts.diff(-1).abs())
 				gdf.drop(['current_loc', 'next_loc'], axis=1, inplace=True)
 				return gdf
@@ -52,7 +67,7 @@ def calculate_velocity(gdf):
 
 def calculate_distance_traveled(gdf):
 				'''
-				Return given dataframe with an extra velocity column that is calculated using the distance covered in a given amount of time
+				Returns given dataframe with an added column ('distance') that contains the accumulated distance travel up to a specific point
 				'''
 				gdf['current_loc'] = gdf.geom.apply(lambda x: (x.x,x.y))
 				gdf['next_loc'] = gdf.geom.shift(-1)
