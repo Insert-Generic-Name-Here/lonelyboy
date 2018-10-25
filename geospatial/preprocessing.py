3c9422b647f57a939e4a004f30d28a31b709f76c
theo <giorgostheo97@gmail.com>
2018-10-25 17:13:50 +0300

    Added function that calculates velocity based on distance traveled during timeframe

--- geospatial/preprocessing.py
+++ geospatial/preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from haversine import haversine
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from random import choice
@@ -33,3 +34,30 @@ def resample_geospatial(sample_ves, rule = '60S', method='linear', crs = {'init'
 				if drop_lon_lat:
 								interpolated.drop(['lat', 'lon'], axis=1)
 				return gpd.GeoDataFrame(interpolated, crs = crs, geometry='geom')
+
+
+def calculate_velocity(gdf):
+				'''
+				Return given dataframe with an extra velocity column that is calculated using the distance covered in a given amount of time
+				'''
+				gdf['current_loc'] = gdf.geom.apply(lambda x: (x.x,x.y))
+				gdf['next_loc'] = gdf.geom.shift(-1)
+				gdf = gdf[:-1]
+				gdf['next_loc'] = gdf.next_loc.apply(lambda x : (x.x,x.y))
+				gdf['velocity'] = gdf[['current_loc', 'next_loc']].apply(lambda x : haversine(x[0], x[1])*0.539956803 , axis=1).multiply(3600/gdf.ts.diff(-1).abs())
+				gdf.drop(['current_loc', 'next_loc'], axis=1, inplace=True)
+				return gdf
+				
+
+
+def calculate_distance_traveled(gdf):
+				'''
+				Return given dataframe with an extra velocity column that is calculated using the distance covered in a given amount of time
+				'''
+				gdf['current_loc'] = gdf.geom.apply(lambda x: (x.x,x.y))
+				gdf['next_loc'] = gdf.geom.shift(-1)
+				gdf = gdf[:-1]
+				gdf['next_loc'] = gdf.next_loc.apply(lambda x : (x.x,x.y))
+				gdf['distance'] = gdf[['current_loc', 'next_loc']].apply(lambda x : haversine(x[0], x[1])*0.539956803 , axis=1).cumsum()
+				gdf.drop(['current_loc', 'next_loc'], axis=1, inplace=True)
+				return gdf
